5242b89ec221ce8cb7c69e4d2ba31b057df5b843
reindert-vetter <reindertvetter@gmail.com>
2025-06-01 23:08:43 +0200

    Fix db path

--- fileloader.php
+++ fileloader.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 require_once 'Src/Helpers/utils.php';
 
 // This file does nog always exists
-if (file_exists('/var/resources/confetti-cms__structure/model.php')) {
-    require_once '/var/resources/confetti-cms__structure/model.php';
+if (file_exists('/var/resources/confetti-cms__db/model.php')) {
+    require_once '/var/resources/confetti-cms__db/model.php';
 }
